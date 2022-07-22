683df802a06a3a51d90c8feae204060f4411b3b5
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-07-21 21:51:55 -0700

    commit tree aeab37bd3a10add405d523c4551fa3b90d90667b

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,21 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(input)
+
+    stack = Stack.new
+
+    input.each_char do |paren|
+        if paren == "("
+            stack.push(paren)
+        else
+            if stack.size == 0 || stack.peek() == ")"
+                stack.push(paren)
+            else
+                stack.pop()
+            end
+        end
+    end
+
+    stack.size
+
+end
